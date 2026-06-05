c6ffadcba864deb64f1c1b32772b0711eb275129
cheetovv <andresbaraguera6@gmail.com>
2026-06-05 18:49:53 -0500

    chore: recreate backend with spring boot 3

---
+++ backend/src/main/java/com/andres/team_event_planner/TeamEventPlannerApplication.java
@@ -0,0 +1,13 @@
+package com.andres.team_event_planner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamEventPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamEventPlannerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/andres/team_event_planner/TeamEventPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andres.team_event_planner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamEventPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
